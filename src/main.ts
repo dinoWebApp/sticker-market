b3b095565d040c29f1d91a43d07a68a500f0008c
nomadWebDeveloper <kgjsmart1559@gmail.com>
2024-11-07 23:36:35 +0900

    메인 화면 퍼블리싱

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // hbs 설정
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  // Partials 디렉토리 등록
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+
+  // 정적 파일 제공 디렉토리 설정
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(3000);
 }
 bootstrap();
